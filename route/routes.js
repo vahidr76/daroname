c3bbbd42a6d6f3f1374ff7ed4b7f01fb36599a5c
vahidr76 <vahidrahimi76@gmail.com>
2017-08-03 14:09:35 +0000

    Update routes.js

--- route/routes.js
+++ route/routes.js
@@ -12,6 +12,7 @@ express.get("/", function(req, res){
 
 express.get("/suggest/:name", controller.suggest);
 express.get("/search/:name/page/:page", controller.search);
+express.get("/posts/:page", controller.posts);
 express.get("/post/:id", controller.post);
 express.post("/insert", controller.insert);
 express.post("/update", controller.update);
